cef1d246908dab02c8f2a2aa825c03eec015eadd
ragels4 <87246484+ragels4@users.noreply.github.com>
2025-09-03 10:33:04 +0300

    Add files via upload
    first lesson of programming in tthk

---
+++ lessonTTHK.py
@@ -0,0 +1,38 @@
+print (6+7)
+print ("6"+"7")
+# "hello world" - str
+# 1 - int
+# 1.5 - float
+
+# muutujad
+name = "Dominik"
+surname = "Ragels"
+
+print (name)
+print (surname)
+print (name+ " " + surname)
+print (" ")
+
+show="Breaking Bad,"
+year="Released in 2008."
+producer="The producer is Vince Galligan."
+actor1="The first actor is Bryan Cranston, "
+actor2="the second actor is Aaron Paul"
+
+print (show+" "+year + " " +producer + " " + actor1 + "and" " " +actor2)
+
+
+# luua muutujad film, aasta, rezisöör, näitleja1, ja näitleja 2
+# tulemus - Film breaking bad ilmus aastal 2008, rezisöör on Vince Galligan,
+# ning peanäitlejad on Bryan Cranston ja Aaron Paul
+
+favanimal = input("Input ur fav animal: ")
+print("My fav animal is: " + favanimal)
+
+height = float(input("Input height "))
+width = float(input("Input width "))
+circumfrence = 2 * (height + width)
+area = (height * width)
+
+print ("circumfrence: ", circumfrence)
+print ("width: ", width)
