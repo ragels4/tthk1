32f886c91c44f56e8fa0f94d595cbdc12f07ee34
ragels4 <87246484+ragels4@users.noreply.github.com>
2025-09-16 18:12:36 +0000

    Add files via upload
    the BASICS of pythoniga

---
+++ PYTHONGUIDE.py
@@ -0,0 +1,29 @@
+print("VARIABLE GUIDE STR")
+per = 123           #Created a variable and assigned an integer of value 123
+per = str(per)      #Converted that variable to a string
+print(per)          #Printed the variable which is now a string, so in text
+
+print("")
+
+print("VARIABLE GUIDE INT")
+var = "88"          #Created a variable and assigned a string of value "88" which is a text
+var = int(var)      #Converted that variable into an integer, which is now a number
+print(var)          #Printed the variable which is now an integer, so a number
+
+print("")
+
+print("/ AND % GUIDE")
+
+print("10 / 3 ")
+equation = 10 / 3   #One / symbol will divide the numbers and give the EXACT float answer
+print(equation)   
+print("")  
+
+print("10 // 3 ")
+equation2 = 10 // 3 #Two // symbols will divide the numbers and round DOWN the answer to the nearest whole number
+print(equation2)
+print("")
+
+print("10 % 4 ")
+protsent = 10 % 4  #The % symbol will show u the remainder of a division equation
+print(protsent)
